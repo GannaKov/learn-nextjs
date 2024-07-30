4810f2c36ae54349b9e15b30b2d5928034207a36
GannaKov <zlatta2000@gmail.com>
2024-07-30 20:02:50 +0200

    Pagination, Search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -8,17 +9,23 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Debounced search term: ${term}`);
+  //console.log(`Searching... ${term}`);
+ 
     const params = new URLSearchParams(searchParams);
-      if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-      }
-    console.log(params.toString())
-    replace(`${pathname}?${params.toString()}`);
+    params.set('page', '1');
+  if (term) {
+    params.set('query', term);
     
+  } else {
+    params.delete('query');
   }
+    console.log(`Patams...`, typeof params);
+  replace(`${pathname}?${params.toString()}`);
+  }, 300);
+  
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
